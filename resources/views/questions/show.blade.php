a8cb618dc5f90b640cc815eac8ceac657027cb7f
Kento-Hongo <gojstr1835262@gmail.com>
2023-11-09 11:00:53 +0900

    answersと書かれた部分はなくしたはずなのにanswersとエラーが出る

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -9,11 +9,16 @@
             <div class="content__question">
                 
                 <p>{{ $question->question }}</p>
+                <p>{{ $question->hint }}</p>
             </div>
-            <div class="answer">
-                <p>{{ $answer->answer }}</p>
+            <div class="answer_area">
+                @foreach ($answers as $answer)
+                    <div class="answer_index">
+                        <p class='answer'>{{ $answers->answer }}</p>
+                    </div>
+                @endforeach
                 <textarea name="answer[answer]" placeholder="?"></textarea>
-                <input type="submit" value="store"/>
+                <input type="submit" value="Answer_submit"/>
             </div>
         </div>
         <div class="footer">
